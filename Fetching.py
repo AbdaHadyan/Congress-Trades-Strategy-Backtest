f75765c1d68f5d30cff70d02767ae98f6fc7d8b7
AbdiAthaillahHadyan <abdi.athaillah.hadyan@gmail.com>
2025-07-07 17:13:40 +0100

    Update Fetching.py

--- Fetching.py
+++ Fetching.py
@@ -1,44 +1,42 @@
+# Fetching
 
 # importing
 
 import pandas as pd
-import seaborn as sns
-import yfinance as yf
 import requests
 from bs4 import BeautifulSoup
 
 # data fetching
 
-page = 1
 all_trade_data = [['Politician', 'Traded Issuer', 'Published', 'Traded', 'Filed After', 'Owner', 'Type', 'Size', 'Price', '']]
-columns = ['Politician', 'Traded Issuer', 'Published', 'Traded', 'Filed After', 'Owner', 'Type', 'Size', 'Price', '']
-for i in range(10) :
-    print(f"scraping page {page}")
-    url = f"https://www.capitoltrades.com/trades?page={page}"
+headers = ['Politician', 'Traded Issuer', 'Published', 'Traded', 'Filed After', 'Owner', 'Type', 'Size', 'Price', ''] 
+
+page = 1
+while True:
+    print(f"scraping page {page}") 
+    url = f"https://www.capitoltrades.com/trades?assetType=stock&page={page}" # URL from Capitol Trades, filtered to remove non-stock trades
     response = requests.get(url)
 
-    soup = BeautifulSoup(response.text, "html.parser")
-    rows = soup.find_all("tr")
-    trade_data = []
+    soup = BeautifulSoup(response.text, "html.parser") # Parses HTML
+    rows = soup.find_all("tr") # Finds Trade rows
+    page_data = []
 
     for row in rows:
-        cells = row.find_all(["td","th"])
-        data = [cell.text.strip() for cell in cells]
-        if len(data) == 10 and data != columns :
-            trade_data.append(data)
+        cells = row.find_all("td")
+        data = [cell.text.strip() for cell in cells] # Removes all whitespace characters in the html
+        if len(data) == 10 and data != headers: # Filters bad rows, and headers from each page
+            page_data.append(data)
     
-    if not trade_data :
+    if not page_data: # Stops loop if page is empty
         break
     
-    
-    all_trade_data.extend(trade_data)
+    all_trade_data.extend(page_data)
 
     page +=1
 
 df = pd.DataFrame(all_trade_data)
 
-df.columns = df.iloc[0]
-df = df[1:].drop(columns="")
-print(df)
+df.columns = df.iloc[0] # creates header
+df = df[1:].drop(columns='') # removes unused column
 
-df.to_csv("All_Trade_Data.csv")
+df.to_csv("All_Trade_Data.csv") 
